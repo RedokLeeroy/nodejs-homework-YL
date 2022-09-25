40aa195e97ebe634e410a33e7a181022320f77f6
RedokLeeroy <Lys1999@ukr.net>
2022-09-25 21:03:40 +0300

    йой

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { DB_PASS } = process.env;
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+mongoose
+  .connect(DB_PASS)
+  .then(() => console.log("database connected successfuly"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
